2e1e82e18073b0ee8aa891d894aa73b90f503fb8
mdamapong <damapongm@gmail.com>
2025-06-27 13:04:59 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mickey Damapong on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
